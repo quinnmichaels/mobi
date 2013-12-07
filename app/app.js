baa4821ca0210b226251069e82d9e3dfdc26c0a2
Quinn Michaels <quinnmichaels@gmail.com>
2013-12-07 10:01:07 -0800

    dev updates

--- app/app.js
+++ app/app.js
@@ -1,13 +1,69 @@
 var http = require('http'),
 	https = require('https'),
-	request = require('request'),
+	path = require('path'),
 	express = require('express'),
+	exphbs = require('express3-handlebars'),
+	request = require('request'),
     mongo = require('mongodb'),
+	monk = require('monk'),
+	crypto = require('crypto'),
+	db = monk('localhost:27017/mobi'),
     app = express(),
+	hbs = exphbs.create({ /* config */ }),
+	routes = require('./routes'),
+	user = require('./routes/user'),
     port = 9300;
+
+app.enable('view cache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// build basic site interactions
+// validate key
+// team stuff
+// user stuff
+
+app.get('/userlist', routes.userlist(db));
+
+// returns a new private key
+app.get('/key', function(req, res) {
+	var str = new Date().toString() + Math.floor(Math.random()*1000000 * Math.PI()).toString();
+	res.send(crypto.createHash('md5').update(str).digest('hex'));
+});
+
+app.get('/enter', function(req, res) {
+	function newKey() {
+		return crypto.createHash('md5').update(new Date().toString() + Math.floor(Math.random()*1000000).toString()).digest('hex');
+	}
+	var newKeys = {
+		user: newKey(),
+		team: newKey()
+	};
+
+	res.render('enter', { 	layout: 'main',
+							title: 'Express',
+							keys: newKeys });
+});
+
+// sync stuff
+
+app.get('/sites', function(req, res) {
+	request('http://www.google.com', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body) // Print the google web page.
+			res.end(body);
+		}
+	})
+});
 
 // serve static files first
 app.use(express.static('public'));
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 
 app.listen(port);
 
---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.send(docs);
+        });
+    };
+};
+
